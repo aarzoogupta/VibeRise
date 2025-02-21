0bcadfd128b5c77101762b92b3f75e39c31b60ef
Shalini <122449558+shalini1008@users.noreply.github.com>
2025-02-22 00:08:45 +0530

    Update Footer.jsx

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="text-white py-4 w-full text-center fixed bottom-0 border border-white-700">
+    <footer className="text-white py-4 w-full text-center  border border-white-700">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-6">
         <p className="text-sm">&copy; {new Date().getFullYear()} VibeRise. All Rights Reserved.</p>
         <div className="flex space-x-6">

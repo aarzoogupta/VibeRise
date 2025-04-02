363beecef139304ba0ea72bbee326f3c6c7098f5
Shalini <122449558+shalini1008@users.noreply.github.com>
2025-04-02 22:48:21 +0530

    Update FirebaseConfig.jsx

--- src/Firebase/FirebaseConfig.jsx
+++ src/Firebase/FirebaseConfig.jsx
@@ -1,22 +1,22 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
 import {getFirestore} from "firebase/firestore"
 import {getStorage} from "firebase/storage"
 
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDmv-m7AVZTYaT8kP9Q5dSHJmAdLtJ1hFM",
-    authDomain: "viberise-d5f75.firebaseapp.com",
-    projectId: "viberise-d5f75",
-    storageBucket: "viberise-d5f75.firebasestorage.app",
-    messagingSenderId: "885463173700",
-    appId: "1:885463173700:web:a2cbd91cc862d692e80a11",
-    measurementId: "G-L92VLF7NZQ"
-  };
+  apiKey: "AIzaSyDGBWdJ_aRh9W-44fj4Ql9afq8BkG9CDxQ",
+  authDomain: "viberisewithouttest.firebaseapp.com",
+  projectId: "viberisewithouttest",
+  storageBucket: "viberisewithouttest.firebasestorage.app",
+  messagingSenderId: "651979665303",
+  appId: "1:651979665303:web:f7a6171cc0fe28d163ded2"
+};
 
   // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);

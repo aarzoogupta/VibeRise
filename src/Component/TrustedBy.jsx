46ef5b458e90b332cb939747ff84198ad513ba73
shalini_10080 <shalini7217625537@gmail.com>
2025-04-05 03:25:30 +0530

    some pics

--- src/Component/TrustedBy.jsx
+++ src/Component/TrustedBy.jsx
@@ -7,8 +7,8 @@ const TrustedBy = () => {
         <h2 className="text-2xl font-bold mb-4 text-purple-600">Trusted By</h2>
         <div className="flex justify-center space-x-8">
           <img src="/partner1.png" alt="Partner 1" className="h-12" />
-          <img src="/partner2.png" alt="Partner 2" className="h-12" />
-          <img src="/partner3.png" alt="Partner 3" className="h-12" />
+          <img src="/partner3.png" alt="Partner 2" className="h-12" />
+          <img src="/Partner2.png" alt="Partner 3" className="h-12" />
           
         </div>
       </div>
